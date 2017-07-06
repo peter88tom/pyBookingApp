59963738edf1ebd9fc0d84a1af539e5271b65ae9
r00t <peter88tom@gmail.com>
2017-07-06 15:28:26 +0200

    Template setup

--- home.py
+++ home.py
@@ -1,9 +1,18 @@
-from flask import flask
+from flask import Flask, render_template, session, url_for, redirect, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'Hello booking App'
+	if 'username' in session:
+		return 'You are logged in  as ' + session['username']
+
+	return render_template('login.html')
+
+
+# Login page
+@app.route('/login')
+def login():
+	return render_template('login.html')
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
